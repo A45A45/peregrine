95eb734dc9f486d8b774832f0842d48e857db0df
Asil <asilakyol454545@gmail.com>
2026-09-20 14:08:51 +0300

    working ig

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include <gtk/gtk.h>
+#include <webkit/webkit.h>
+
+static void activate(GtkApplication *app, gpointer user_data) {
+    const char *url = (const char *)user_data;
+
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Peregrine");
+    gtk_window_set_default_size(GTK_WINDOW(window), 1024, 768);
+
+    GtkWidget *web_view = webkit_web_view_new();
+    gtk_window_set_child(GTK_WINDOW(window), web_view);
+
+    webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web_view), url);
+    gtk_window_present(GTK_WINDOW(window));
+}
+
+int main(int argc, char **argv) {
+    const char *url = (argc > 1) ? argv[1] : "https://webkitgtk.org";
+
+    GtkApplication *app = gtk_application_new("org.peregrine.browser", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), (gpointer)url);
+
+    int status = g_application_run(G_APPLICATION(app), 0, NULL);
+    g_object_unref(app);
+    return status;
+}
